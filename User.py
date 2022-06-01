159b4aaf1b6a686ac39f2ab9d863d87df8891d2d
kineticvenom <kalebvarnes@yahoo.com>
2022-06-01 10:16:32 -1000

    Starter class code

--- User.py
+++ User.py
@@ -1,1 +1,13 @@
 # your improved User class goes here
+class Users: 
+    def __init__(self, user_name, first_name=None, last_name=None, email_address=None, password) :  
+        self.user_name = user_name
+        self.name = first_name + " " + last_name
+        self.email = email_address
+        self._password = password
+    
+    def __str__(self):
+        return f"Username: {self.user_name} Name:{self.name} Email:{self.email}"
+
+
+Kinetic = Users('kinetic', 'kaleb', 'kalebvarnes@yahoo.com', 'kineticvenom')
