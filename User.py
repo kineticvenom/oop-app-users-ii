0837229295818e78fd8e1d45038bd9f84786ba69
kineticvenom <kalebvarnes@yahoo.com>
2022-06-01 13:09:44 -1000

    Working Code, Need to add Delete Post

--- User.py
+++ User.py
@@ -1,13 +1,34 @@
 # your improved User class goes here
+from datetime import date, datetime
 class Users: 
-    def __init__(self, user_name, first_name=None, last_name=None, email_address=None, password) :  
+    user_post={}
+    def __init__(self, user_name, password, first_name=None, last_name=None, email_address=None) :  
         self.user_name = user_name
         self.name = first_name + " " + last_name
         self.email = email_address
         self._password = password
-    
+        self.posts = []
+        Users.user_post[user_name] =[]
+
     def __str__(self):
-        return f"Username: {self.user_name} Name:{self.name} Email:{self.email}"
+        return str(f"Hello you can call me {self.user_name}")
 
+    current_time = datetime.now().strftime("%d/%m/%Y %H:%M")
+    
 
-Kinetic = Users('kinetic', 'kaleb', 'kalebvarnes@yahoo.com', 'kineticvenom')
+    def NewUser_post(self, str):
+
+        post = f"Username: {self.user_name} posted:  {str} on {Users.current_time}"
+        self.posts.append(post)
+        Users.user_post[self.user_name].append(post)
+        return post
+        
+
+Kinetic = Users('kinetic','kineticvenom', 'kaleb',"varnes", 'kalebvarnes@yahoo.com')
+Absol = Users('Raygo', "slipme", 'jimmy', 'brown', 'jimmybrown@gmail.com')
+
+test = Kinetic.NewUser_post("This is starting to make sense!")
+test2 = Absol.NewUser_post(" Maybe I  should play video games next?")
+all_posts = Users.user_post
+
+print(all_posts)
